16abb1d27af45049bc1cbceb720b209553fc7ff3
sinan-ipek <116756651+sinan-ipek@users.noreply.github.com>
2026-08-11 23:24:30 +0200

    Fix Compose weight import

--- app/src/main/java/com/stereopairfinder/MainActivity.kt
+++ app/src/main/java/com/stereopairfinder/MainActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
